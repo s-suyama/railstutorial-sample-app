e3776aed963eb6457aa319353b9132d2d192f434
shogo-suyama <suman.yarn@gmail.com>
2016-11-17 23:13:54 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -68,6 +70,11 @@ class User < ActiveRecord::Base
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # 試作feedの定義
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
